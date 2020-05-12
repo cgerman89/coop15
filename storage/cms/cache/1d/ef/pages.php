66bf8bb3e6e90ad480ac42b6b8056f53d7d68e83
cgerman89 <cgerman.89@outlook.com>
2020-05-12 15:55:40 -0500

    migrando a pruebas

---
+++ storage/cms/cache/1d/ef/pages.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5eb3196fe4fa4033227096_30ef22e916d3d415726afc8ec48081e1Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/twig/92/92868a48ad853f6fd2a4f7719f44008b8e63368950a310db22d7e7e9302be7dc.php
@@ -0,0 +1,52 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* C:\laragon\www\new15/themes/coop152020_2/content/static-pages/vision.htm */
+class __TwigTemplate_ade6ca3003641ffe2a59f0a5ccad978c4db6a158a22a5088e4a3256eb4ac107c extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    public function getTemplateName()
+    {
+        return "C:\\laragon\\www\\new15/themes/coop152020_2/content/static-pages/vision.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "C:\\laragon\\www\\new15/themes/coop152020_2/content/static-pages/vision.htm", "");
+    }
+}
